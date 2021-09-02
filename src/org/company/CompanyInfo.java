95d136a37942378aa3b4911c880d1b7cbd9bfaa0
Thenmozhi3125 <honusrithenu@gmail.com>
2021-09-02 23:39:49 +0530

    first

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,28 @@
+package org.company;
+
+public class CompanyInfo {
+	public void companyName() {
+		System.out.println("company Name is:tcs");
+		
+	}
+	public void companyId() {
+		System.out.println("company Id is:1234");
+		
+	}
+	public void companyAddress() {
+		System.out.println("company Address is:adayar");
+		
+	}
+	public static void main(String[] args) {
+		CompanyInfo c = new CompanyInfo();
+		     c.companyName();
+		     c.companyId();
+		     c.companyAddress();
+		
+		
+	}
+	
+	
+	
+
+}
